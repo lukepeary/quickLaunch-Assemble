498a90278f6d6e463dec85c01dcea377f5560114
Luke Peary <lukep@reactive.com>
2015-03-11 17:11:49 +1100

    First Commit

--- gruntfile.js
+++ gruntfile.js
@@ -4,22 +4,35 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          'css/style.css' : 'css/sass/style.scss'
+          'dist/css/style.css' : 'src/css/sass/style.scss'
         }
       }
     },
     browserSync: {
         bsFiles: {
             src : [
-              'css/*.css'
+              'dist/css/*.css',
+              'dist/*.html'
           ]
         },
         options: {
             server: {
-                baseDir: "./"
+                baseDir: "./dist"
             },
             watchTask: true
         }
+    },
+    assemble: {
+      options: {
+        layout: ['default.hbs'],
+        layoutdir: 'src/templates/layouts',
+        partials: ['src/templates/partials/*.hbs'],
+        flatten: true
+      },
+      pages: {
+          src: ['src/templates/pages/*.hbs'],
+          dest: 'dist'
+      }
     },
     watch: {
       css: {
@@ -28,11 +41,43 @@ module.exports = function(grunt) {
         options: {
           spawn: false
         }
+      },
+      html: {
+        files: '**/*.hbs',
+        tasks: ['assemble']
+      }
+    },
+    copy: {
+      images: {
+        expand: true,
+        src: ['src/assets/images/**'],
+        dest: 'dist/assets/images/',
+        filter: 'isFile',
+        flatten: true
+      },
+      fonts: {
+        expand: true,
+        src: ['src/assets/fonts/**'],
+        dest: 'dist/assets/fonts/',
+        filter: 'isFile',
+        flatten: true
+      },
+      scriptLibs: {
+        expand: true,
+        src: ['src/js/vendor/**'],
+        dest: 'dist/js/vendor',
+        filter: 'isFile',
+        flatten: true
       }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browser-sync');
+  // grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('assemble');
   grunt.registerTask('default',['browserSync','watch']);
+  grunt.registerTask('build',['copy']);
 }
